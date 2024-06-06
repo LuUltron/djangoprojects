7d634392cba8b1ec9e57d4abe5f3145089831f75
LuUltron <60403564+LuUltron@users.noreply.github.com>
2024-06-06 12:11:37 +0300

    Add files via upload

--- newtestapp/news/views.py
+++ newtestapp/news/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Articles
+from .forms import ArticlesForm
+from django.views.generic import DetailView, UpdateView, DeleteView
 
 def news_home(request):
     news = Articles.objects.order_by('-date').all()
@@ -9,5 +11,36 @@ def news_home(request):
     }
     return render(request, 'news/news_home.html', data)
 
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'news/details_view.html'
+    context_object_name = 'article'
+
+class NewsUpdateView(UpdateView):
+    model = Articles
+    template_name = 'news/create.html'
+
+    form_class = ArticlesForm
+
+class NewsDeleteView(DeleteView):
+    model = Articles
+    success_url = '/news'
+    template_name = 'news/news-delete.html'
+
+
 def create(request):
-    return render(request, 'news/create.html')
+    error = ''
+    if request.method == 'POST':
+        form = ArticlesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Forma byla nevernoi'
+
+    form = ArticlesForm()
+    data = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'news/create.html', data)
